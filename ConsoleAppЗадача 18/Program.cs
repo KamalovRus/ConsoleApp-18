428c610c26a933aeb947b8fe8d70c81ac06007a7
kamru <kamru@LAPTOP-KUCADU9B>
2021-12-18 16:44:30 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppЗадача 18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckBalanced("(}())[]"));
+        }
+        public static bool CheckBalanced(String check)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < check.Length; i++)
+            {
+                char character = check[i];
+                if (character == '[' || character == '{' || character == '(')
+                    stack.Push(character);
+                else if (character == ']' || character == '}' || character == ')')
+                {
+                    if (!stack.Any())
+                        return false;
+                    switch (character)
+                    {
+                        case ']':
+                            if (stack.Pop() != '[')
+                                return false;
+                            break;
+                        case '}':
+                            if (stack.Pop() != '{')
+                                return false;
+                            break;
+                        case ')':
+                            if (stack.Pop() != '(')
+                                return false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            // проверка, что стек пуст, значит строка сбалансирована
+            if (!stack.Any())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
